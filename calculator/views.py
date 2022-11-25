46b308c9222f1a4e380917e2a89929eb992e63fa
malkolmz <kurban.63.axmedov@gmail.com>
2022-11-25 17:25:05 +0400

    hm_django_2

---
+++ calculator/views.py
@@ -0,0 +1,69 @@
+from django.shortcuts import render
+
+# Create your views here.
+from django.shortcuts import render
+
+DATA = {
+    'omlet': {
+        'яйца, шт': 2,
+        'молоко, л': 0.1,
+        'соль, ч.л.': 0.5,
+    },
+    'pasta': {
+        'макароны, г': 0.3,
+        'сыр, г': 0.05,
+    },
+    'buter': {
+        'хлеб, ломтик': 1,
+        'колбаса, ломтик': 1,
+        'сыр, ломтик': 1,
+        'помидор, ломтик': 1,
+    },
+    # можете добавить свои рецепты ;)
+}
+
+
+# Напишите ваш обработчик. Используйте DATA как источник данных
+# Результат - render(request, 'calculator/omlet.html', context)
+# В качестве контекста должен быть передан словарь с рецептом:
+# context = {
+#   'recipe': {
+#     'ингредиент1': количество1,
+#     'ингредиент2': количество2,
+#   }
+# }
+def omlet(request):
+    servings = int(request.GET.get("servings", 1))
+
+    context = {
+        'omlet': {
+            'яйца, шт': 2*servings,
+            'молоко, л': 0.1*servings,
+            'соль, ч.л.': 0.5*servings,
+        },
+    }
+    return render(request, 'calculator/omlet.html', context)
+
+
+def buter(request):
+    servings = int(request.GET.get("servings", 1))
+    context = {
+        'buter': {
+            'хлеб, ломтик': 1 * servings,
+            'колбаса, ломтик': 1 * servings,
+            'сыр, ломтик': 1 * servings,
+            'помидор, ломтик': 1 * servings,
+        },
+    }
+    return render(request, 'calculator/buter.html', context)
+
+
+def pasta(request):
+    servings = int(request.GET.get("servings", 1))
+    context = {
+        'pasta': {
+            'макароны, г': 0.3*servings,
+            'сыр, г': 0.05*servings,
+        },
+    }
+    return render(request, 'calculator/pasta.html', context)
